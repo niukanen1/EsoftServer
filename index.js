119abd2d8897c53bde6f3677fd90c29343cfa7c9
ivanNiukanen <ivanniu2003@gmail.com>
2021-12-16 01:09:37 +0200

    fix1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 
 const {typeDefs} = require('./Schema/typeDefs/UserType'); 
 const {resolvers} = require('./Schema/Resolvers/UserResolvers');
-const Schema = {typeDefs, resolvers};
+const Schema = {typeDefs, resolvers, introspection: process.env.NODE_ENV !== 'production'};
 
 const server = new ApolloServer(Schema); 
 
